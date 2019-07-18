cfd521f00d88b17836b086a8d30c410d40109aef
nursh <nsheikhmohammed01@gmail.com>
2019-07-18 15:51:08 -0400

    Added Test for Spring Boot Application to see if it works in CI

--- backend/web/src/main/java/pitalo/web/PitaloApplication.java
+++ backend/web/src/main/java/pitalo/web/PitaloApplication.java
@@ -2,14 +2,8 @@ package pitalo.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = {"pitalo"})
-@EntityScan(basePackages = {"pitalo"})
-@ComponentScan(basePackages = {"pitalo"})
 public class PitaloApplication {
 
     public static void main(String[] args) {
---
+++ backend/web/src/test/java/pitalo/web/PitaloApplicationTest.java
@@ -0,0 +1,13 @@
+package pitalo.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitaloApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
