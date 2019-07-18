b967483586119288610f151646d9478bfa4525b7
nursh <nsheikhmohammed01@gmail.com>
2019-07-18 09:46:07 -0400

    Fixed multi module project Setup

--- backend/web/src/main/java/pitalo/web/PitaloApplication.java
+++ backend/web/src/main/java/pitalo/web/PitaloApplication.java
@@ -2,8 +2,14 @@ package pitalo.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"pitalo"})
+@EntityScan(basePackages = {"pitalo"})
+@ComponentScan(basePackages = {"pitalo"})
 public class PitaloApplication {
 
     public static void main(String[] args) {
